2c5c1b2a52d20fb6fbcc1e20462f422b88d0d25f
Maira Freitas <maira.oliveirafreitas@gmail.com>
2021-07-17 20:43:25 -0300

    pdf merge

---
+++ pdf_merge.py
@@ -0,0 +1,13 @@
+import sys
+import PyPDF2
+
+input = sys.argv[1:]
+
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        print(pdf)
+        merger.append(pdf)
+    merger.write('super.pdf')
+
+pdf_combiner(input)
